13ee1ec598e267f9537907a7cc5c0512d6b66200
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-09-01 16:04:44 -0700

    Update LWJGL from 2 to 3; use StartupHelper.
    While the glfw-awt-macos option exists, and so does the glfw_async configuration, the most user-friendly results on MacOS seem to come from actually setting -XstartOnFirstThread. StartupHelper does this, though it might still have issues with Graal in some way. StartupHelper also gets LWJGL3 to actually work when a user has non-ASCII chars in their username.

--- desktop/src/de/swagner/paxbritannica/desktop/DesktopLauncher.java
+++ desktop/src/de/swagner/paxbritannica/desktop/DesktopLauncher.java
@@ -1,15 +1,15 @@
 package de.swagner.paxbritannica.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import de.swagner.paxbritannica.PaxBritannica;
 
 public class DesktopLauncher {
 	public static void main(String[] args) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1024;
-		config.height = 550;
-		config.title = "Pax Britannica";
-		new LwjglApplication(new PaxBritannica(), config);
+		if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowedMode(1024, 550);
+		config.setTitle("Pax Britannica");
+		new Lwjgl3Application(new PaxBritannica(), config);
 	}
 }
